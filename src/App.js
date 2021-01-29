fd971d456f123323a857a627b1e98c89a75c0876
Arindam <msaha6368@gmail.com>
2021-01-29 23:50:22 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Production Website</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
