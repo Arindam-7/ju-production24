711d431290215fbb0e917362eb8fcc9ba9369b55
Arindam-7 <msaha6368@gmail.com>
2021-01-31 19:12:38 +0530

    added process env name to browserRouter

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <>
         
-      <BrowserRouter>
+      <BrowserRouter basename= {process.env.public_url} >
         <Navbar />
         <Home />
         <Switch>
